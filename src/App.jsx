2c5a1195c97c23a4dcb9724fb810c4a0305a644c
Pedro Braude <pedrobraude@gmail.com>
2024-04-28 10:12:53 -0300

    feat: add ability to add items to shopping cart

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import { useState } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 import Header from './components/Header';
 import Footer from './components/Footer';
@@ -8,17 +10,37 @@ import { db } from './data/db';
 
 export default function App() {
   const [data, setData] = useState(db);
+  const [cart, setCart] = useState([]);
+  const notify = (message) => toast(message);
+
+  const addToCart = (item) => {
+    const itemExists = cart.findIndex((guitar) => guitar.id === item.id);
+
+    if (itemExists >= 0) {
+      // add elements with a state copy
+      const updatedCart = [...cart];
+
+      updatedCart[itemExists].quantity++; // itemExists is the  item position.
+      setCart(updatedCart);
+      notify(`Ahora tienes ${item.quantity} "${item.name}" en  el carrito`);
+    } else {
+      item.quantity = 1;
+      setCart([...cart, item]);
+      notify(`Ahora tienes ${item.quantity} "${item.name}" en  el carrito`);
+    }
+  };
 
   return (
     <>
-      <Header />
+      <ToastContainer position="top-right" closeOnClick theme="light" />
+      <Header cart={cart} />
       <main className="container-xl mt-5">
         <h2 className="text-center">Nuestra Colección</h2>
 
         <div className="row mt-5">
           {data.map((guitar) => (
             //  only with parenthesis you have an implicity return
-            <Guitar key={guitar.id} {...guitar} />
+            <Guitar key={guitar.id} guitar={guitar} addToCart={addToCart} />
           ))}
         </div>
       </main>
--- src/components/Guitar.jsx
+++ src/components/Guitar.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
+export default function Guitar({ guitar, addToCart }) {
+  const { id, image, name, description, price } = guitar;
 
-export default function Guitar({ image, name, description, price }) {
   return (
     <>
       <div className="col-md-6 col-lg-4 my-4 row align-items-center">
@@ -15,7 +15,11 @@ export default function Guitar({ image, name, description, price }) {
           <h3 className="text-black fs-4 fw-bold text-uppercase">{name}</h3>
           <p>{description}</p>
           <p className="fw-black text-primary fs-3">$ {price}</p>
-          <button type="button" className="btn btn-dark w-100">
+          <button
+            type="button"
+            className="btn btn-dark w-100"
+            onClick={() => addToCart(guitar)}
+          >
             Agregar al Carrito
           </button>
         </div>
