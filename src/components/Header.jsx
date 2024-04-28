89587a9edae7132d5af1569db3d01bb3a2c1c72d
Pedro Braude <pedrobraude@gmail.com>
2024-04-28 10:22:49 -0300

    demo: show how to implement derived state in Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,8 @@
-export default function Header() {
+export default function Header({ cart }) {
+  // State derivado
+  const isEmpty = () => cart.length === 0;
+  const cartTotal = () =>
+    cart.reduce((total, item) => total + item.quantity * item.price, 0);
   return (
     <>
       <header className="py-5 header">
@@ -22,48 +26,53 @@ export default function Header() {
                 />
 
                 <div id="carrito" className="bg-white p-3">
-                  <p className="text-center">El carrito esta vacio</p>
-                  <table className="w-100 table">
-                    <thead>
-                      <tr>
-                        <th>Imagen</th>
-                        <th>Nombre</th>
-                        <th>Precio</th>
-                        <th>Cantidad</th>
-                        <th></th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      <tr>
-                        <td>
-                          <img
-                            className="img-fluid"
-                            src="./img/guitarra_02.jpg"
-                            alt="imagen guitarra"
-                          />
-                        </td>
-                        <td>SRV</td>
-                        <td className="fw-bold">$299</td>
-                        <td className="flex align-items-start gap-4">
-                          <button type="button" className="btn btn-dark">
-                            -
-                          </button>
-                          1
-                          <button type="button" className="btn btn-dark">
-                            +
-                          </button>
-                        </td>
-                        <td>
-                          <button className="btn btn-danger" type="button">
-                            X
-                          </button>
-                        </td>
-                      </tr>
-                    </tbody>
-                  </table>
+                  {isEmpty() ? (
+                    <p className="text-center">El carrito esta vacio</p>
+                  ) : (
+                    <table className="w-100 table">
+                      <thead>
+                        <tr>
+                          <th>Imagen</th>
+                          <th>Nombre</th>
+                          <th>Precio</th>
+                          <th>Cantidad</th>
+                          <th></th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        {cart.map(({ id, image, price, quantity, name }) => (
+                          <tr key={id}>
+                            <td>
+                              <img
+                                className="img-fluid"
+                                src={`./img/${image}.jpg`}
+                                alt="imagen guitarra"
+                              />
+                            </td>
+                            <td>{name}</td>
+                            <td className="fw-bold">${price}</td>
+                            <td className="flex align-items-start gap-4">
+                              <button type="button" className="btn btn-dark">
+                                -
+                              </button>
+                              {quantity}
+                              <button type="button" className="btn btn-dark">
+                                +
+                              </button>
+                            </td>
+                            <td>
+                              <button className="btn btn-danger" type="button">
+                                X
+                              </button>
+                            </td>
+                          </tr>
+                        ))}
+                      </tbody>
+                    </table>
+                  )}
 
                   <p className="text-end">
-                    Total pagar: <span className="fw-bold">$899</span>
+                    Total pagar: <span className="fw-bold">{cartTotal()}</span>
                   </p>
                   <button className="btn btn-dark w-100 mt-3 p-2">
                     Vaciar Carrito
