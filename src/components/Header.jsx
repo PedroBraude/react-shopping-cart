e75d9c571dd244653e956ab84e2dcc0064b6c65f
Pedro Braude <pedrobraude@gmail.com>
2024-04-28 10:29:29 -0300

    demo: show how to implement useMemo instead derived state

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,12 @@
+import { useMemo } from 'react';
+
 export default function Header({ cart }) {
   // State derivado
-  const isEmpty = () => cart.length === 0;
-  const cartTotal = () =>
-    cart.reduce((total, item) => total + item.quantity * item.price, 0);
+  const isEmpty = useMemo(() => cart.length === 0, [cart]);
+  const cartTotal = useMemo(
+    () => cart.reduce((total, item) => total + item.quantity * item.price, 0),
+    [cart]
+  );
   return (
     <>
       <header className="py-5 header">
@@ -26,7 +30,7 @@ export default function Header({ cart }) {
                 />
 
                 <div id="carrito" className="bg-white p-3">
-                  {isEmpty() ? (
+                  {isEmpty ? (
                     <p className="text-center">El carrito esta vacio</p>
                   ) : (
                     <table className="w-100 table">
@@ -72,7 +76,7 @@ export default function Header({ cart }) {
                   )}
 
                   <p className="text-end">
-                    Total pagar: <span className="fw-bold">{cartTotal()}</span>
+                    Total pagar: <span className="fw-bold">{cartTotal}</span>
                   </p>
                   <button className="btn btn-dark w-100 mt-3 p-2">
                     Vaciar Carrito
